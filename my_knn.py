5b80e9364b16506f49409672eb3af79e9e5e2a33
Mike Giancola <mjgiancola@wpi.edu>
2017-04-14 14:03:57 -0400

    Updated to use utility file

--- my_knn.py
+++ my_knn.py
@@ -1,5 +1,4 @@
-from read_data import read_data
-from partition_data import partition_data
+from data_utils import * # Utility functions we wrote
 
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
@@ -14,7 +13,7 @@ Model performs around the same for K between 3 and 21. This implies that the cla
 # Regarding the Nearest Neighbors algorithms, if it is found that two neighbors, neighbor k+1 and k, have identical distances but different labels, the results will depend on the ordering of the training data. 
 
 def best_knn():
-	return KNeighborsClassifier(n_neighbors=0) # TODO Hard-code best k value
+	return KNeighborsClassifier(n_neighbors=3) # Empirically, k=3 was best under both 10-fold and LOO CV.
 
 def getScores(X, Y, k_list, num_cv_folds):
 	kfold = KFold(n_splits=num_cv_folds)
@@ -29,13 +28,12 @@ def getScores(X, Y, k_list, num_cv_folds):
 
 if __name__ == '__main__':
 
-	X, Y = read_data()
-	X_tr, y_tr,_,_ = partition_data(X, Y)
+	X, Y = get_training()
 
-	k_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 31, 51, 71, 91]
+	k_list = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 31, 51, 71, 91]
 
-	ten_scores = getScores(X_tr, y_tr, k_list, 10)
-	loocv_scores = getScores(X_tr, y_tr, k_list, len(X_tr))
+	ten_scores = getScores(X, Y, k_list, 10)
+	loocv_scores = getScores(X, Y, k_list, len(X))
 
 	print("____________________________________________________________")
 	for i in range(len(k_list)):
