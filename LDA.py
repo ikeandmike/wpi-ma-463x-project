db9d3502d868b30b6bd038b6e2169dc273fd0ba3
rxgran <rxgran@wpi.edu>
2017-04-14 06:46:25 -0700

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -1,20 +1,18 @@
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import cross_val_score
 
-# Runs LDA and computes the accuracy of the model using cross validation with 10 folds by default.
-# Returns a string with the accuracy +/- 2*std
+# Runs LDA and computes the mean accuracy on the given test data and labels.
 
 
-def LDA(X_train, Y_train, numCVFolds=10):
+def LDA(X_train, Y_train, X_test, Y_test):
 
     LDAtest = LinearDiscriminantAnalysis()
     LDAtest.fit(X_train, Y_train)
     LinearDiscriminantAnalysis(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False, tol=0.0001)
 
-    cvscore = cross_val_score(LDAtest, X_train, Y_train, cv=numCVFolds)
-
-    accuracy = ("Accuracy: %0.2f (+/- %0.2f)" % (cvscore.mean(), cvscore.std() * 2))
-    print accuracy
-    return accuracy
+    Accuracy_train = LDAtest.score(X_train,Y_train,sample_weight=None)
+    Accuracy_test = LDAtest.score(X_test,Y_test,sample_weight=None)
+    
+    print Accuracy_train
+    print Accuracy_test
 
