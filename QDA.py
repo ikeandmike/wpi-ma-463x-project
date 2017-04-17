e1b9caea48340547343b62c7153f3be6d39c6c27
rxgran <rxgran@wpi.edu>
2017-04-16 21:22:48 -0400

    Update QDA.py

--- QDA.py
+++ QDA.py
@@ -1,5 +1,6 @@
+from data_utils import *
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-import numpy as np
 
 # Runs QDA and computes the mean accuracy on the given data and labels 
 
