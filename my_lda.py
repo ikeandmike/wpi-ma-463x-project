c23df716e374dd0d804be8a5b08d3b2e56b4e906
cjlovering <cjlovering@130.215.14.76>
2017-04-17 23:30:19 -0400

    added some non-preprocessing

--- my_lda.py
+++ my_lda.py
@@ -1,12 +1,34 @@
-from data_utils import *
-from my_pca import *
-from my_lasso import *
+from data_utils import get_training
+from my_pca import my_pca
+from my_lasso import my_lasso
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def best_lda():
     #TODO find best LDA using data
     pass
+
+def generate_subsets():
+    """Returns some of the subsets of predictors
+    """
+    out = [ range(30),
+            [0,1,2,3,4,5],
+            [5,6,7,8,9,10],
+            [1,2,3],
+            [21,23,5,6,2],
+            [4,19,12,5,6]]
+    return out
+
+def getScores(X, y, subsets, num_cv_folds):
+    kFold = KFold(n_splits=num_cv_folds)
+    subsets = [X[:, subset] for subset in subsets]
+    scores = []
+    my_lda = LinearDiscriminantAnalysis()
+    for subset in subsets:
+        print "Evaluating LDA with a subset of predictors"
+        my_lda.fit(subset, Y)
+        scores.append(cross_val_score(my_lda, subset, Y, cv = kFold).mean())
+    return scores
 
 def getScores_pca(X, Y, num_pred_list, num_cv_folds):
     kfold = KFold(n_splits=num_cv_folds)
@@ -37,12 +59,24 @@ if __name__ == '__main__':
     X, Y = get_training()
 
     num_pred_list = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 30]
-    alpha_list = [.125, .25, .375, .5, .625, .75, .875, 1]
+    alpha_list = [0,.125, .25, .375, .5, .625, .75, .875, 1]
+    subsets = generate_subsets()
 
+    ten_scores_subsets = getScores(X, Y, subsets, 10)
+    loocv_scores_subsets = getScores(X, Y, subsets, len(X))
+ 
     ten_scores_pca = getScores_pca(X, Y, num_pred_list, 10)
     loocv_scores_pca = getScores_pca(X, Y, num_pred_list, len(X))
     ten_scores_lasso = getScores_lasso(X, Y, alpha_list, 10)
     loocv_scores_lasso = getScores_lasso(X, Y, alpha_list, len(X))
+
+    print("_______________________Subsets______________________________")
+    for i in range(len(subsets)):
+	acc1 = ten_scores_subsets[i]
+	acc2 = loocv_scores_subsets[i]
+	print("| subset = %2d | 10-Fold Accuracy: %.3f | LOOCV Accuracy: %.3f |" % (i, acc1, acc2))
+    print("____________________________________________________________")
+
 
     print("_______________________PCA__________________________________")
     for i in range(len(num_pred_list)):
@@ -57,35 +91,43 @@ if __name__ == '__main__':
 	a = alpha_list[i]
 	acc1 = ten_scores_lasso[i]
 	acc2 = loocv_scores_lasso[i]
-	print("| predictors = %2d | 10-Fold Accuracy: %.3f | LOOCV Accuracy: %.3f |" % (a, acc1, acc2))
+	print("| alpha = %.3f | 10-Fold Accuracy: %.3f | LOOCV Accuracy: %.3f |" % (a, acc1, acc2))
     print("____________________________________________________________")
     # TODO Generate graphs of num_pred vs. accuracy
 
 
 """
 output:
-_______________________PCA__________________________________
-| predictors =  3 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  5 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  7 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  9 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 11 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 13 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 15 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 17 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 19 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 21 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors = 30 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-____________________________________________________________
-_______________________Lasso________________________________
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-| predictors =  1 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
-____________________________________________________________
-
+    _______________________Subsets______________________________
+    | subset =  0 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | subset =  1 | 10-Fold Accuracy: 0.902 | LOOCV Accuracy: 0.914 |
+    | subset =  2 | 10-Fold Accuracy: 0.906 | LOOCV Accuracy: 0.908 |
+    | subset =  3 | 10-Fold Accuracy: 0.885 | LOOCV Accuracy: 0.902 |
+    | subset =  4 | 10-Fold Accuracy: 0.921 | LOOCV Accuracy: 0.929 |
+    | subset =  5 | 10-Fold Accuracy: 0.894 | LOOCV Accuracy: 0.900 |
+    ____________________________________________________________
+    _______________________PCA__________________________________
+    | predictors =  3 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors =  5 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors =  7 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors =  9 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 11 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 13 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 15 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 17 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 19 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 21 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | predictors = 30 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    ____________________________________________________________
+    _______________________Lasso________________________________
+    | alpha = 0.000 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.125 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.250 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.375 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.500 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.625 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.750 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 0.875 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    | alpha = 1.000 | 10-Fold Accuracy: 0.964 | LOOCV Accuracy: 0.960 |
+    ____________________________________________________________
 """
